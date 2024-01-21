4f08fccfb6ad3e1e3462fa173aaa61c28d3bb28a
aleks <aprokhorenkodev@gmail.com>
2024-01-21 14:03:34 +0100

    Update v1.5, new collapsed menu

--- Fotografia/menuScrip.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".menu-toggle").click(function(){
-        $(".ul-nav").toggleClass("show");
-        $(".bar:nth-child(1").toggleClass("rotate-45");
-        $(".bar:nth-child(2").toggleClass("hide");
-        $(".bar:nth-child(3").toggleClass("rotate45");
-    });
-});
